11f87b99beb0aabdb5cb25614fa816c8c9461b49
RCD-XX <carla.zumaran@gmail.com>
2018-05-07 18:51:25 -0300

    Cambios en mobile y estructura de archivos

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 $(document).ready(function() {
-    $('.submenu').hide();
-
-    $('.item').mouseenter(function() {
-        $('.submenu').hide();
-        $(this).children('.submenu').show();
-    })
 })
+
+$('.submenu').hide();
+$('.item').mouseenter(function() {
+  $('.submenu').hide();
+  $(this).children('.submenu').show();
+})
+
+// Funciones efectos mobile
+
+$('.close-menu-side').click(function(){
+  console.log('kol')
+  $('span').removeClass('close-menu-side')
+  $('header').removeClass('active')
+  $('span').addClass('menu-bar')
+  $('#menu-movil').hide()
+})
+
+$('.menu-bar').click(function(){
+  $('header').addClass('active')
+  $('span').addClass('close-menu-side')
+  $('span').removeClass('menu-bar')
+  $('#menu-movil').show()
+})
+
